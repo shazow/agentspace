5c51aaa4fc12ea40517ee606d12d933c5792387c
Andrey Petrov <shazow@gmail.com>
2026-03-12 19:50:01 -0400

    Fix airlock launch context for vmWithAirlock

--- flake.nix
+++ flake.nix
@@ -16,38 +16,61 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      mkSandbox = { extraModules ? [] }: nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          microvm.nixosModules.microvm
-          ./sandbox-qemu.nix
+      mkSandbox =
+        {
+          extraModules ? [ ],
+        }:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            microvm.nixosModules.microvm
+            ./sandbox-qemu.nix
 
-          # Module Configuration
-          {
-            agentspace.sandbox = {
-              enable = true;
-              user = "agent";
-              hostName = "agent-sandbox";
-              protocol = "9p";
+            # Module Configuration
+            {
+              agentspace.sandbox = {
+                enable = true;
+                user = "agent";
+                hostName = "agent-sandbox";
+                protocol = "9p";
 
-              persistence.homeImage = "./home.img";
-              bundle = [ ];
-            };
+                persistence.homeImage = "./home.img";
+                bundle = [ ];
+              };
 
-            # System-specific overrides can still go here
-            nix.registry.nixpkgs.flake = nixpkgs;
-            nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
-            nix.settings.experimental-features = [
-              "nix-command"
-              "flakes"
-            ];
-          }
-        ] ++ extraModules;
-      };
+              # System-specific overrides can still go here
+              nix.registry.nixpkgs.flake = nixpkgs;
+              nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+              nix.settings.experimental-features = [
+                "nix-command"
+                "flakes"
+              ];
+            }
+          ]
+          ++ extraModules;
+        };
+
+      mkLaunchScript =
+        name:
+        let
+          vmConfig = self.nixosConfigurations.${name}.config;
+          runnerPath = vmConfig.microvm.declaredRunner.outPath;
+          script = pkgs.writeShellScriptBin "launch-agent-${name}" ''
+            set -e
+
+            REPO_DIR=$(${pkgs.coreutils}/bin/realpath .)
+
+            ${vmConfig.agentspace.sandbox.initExtra}
+
+            echo "🖥️  Running Agent..."
+            exec "${runnerPath}/bin/microvm-run"
+          '';
+        in
+        script;
     in
     {
       nixosConfigurations = {
-        vm = mkSandbox {};
+        vm = mkSandbox { };
         vmWithAirlock = mkSandbox {
           extraModules = [
             ./airlock.nix
@@ -58,15 +81,11 @@
         };
       };
 
-      packages.${system} =
-        let
-          mkRunner = name: self.nixosConfigurations.${name}.config.microvm.declaredRunner;
-        in
-        {
-          default = mkRunner "vm";
-          vm = mkRunner "vm";
-          vmWithAirlock = mkRunner "vmWithAirlock";
-        };
+      packages.${system} = {
+        default = mkLaunchScript "vm";
+        vm = mkLaunchScript "vm";
+        vmWithAirlock = mkLaunchScript "vmWithAirlock";
+      };
 
       checks.${system} = import ./checks.nix {
         inherit
@@ -81,22 +100,7 @@
         default = self.apps.${system}.launch;
         launch = {
           type = "app";
-          program =
-            let
-              # Access config to make script dynamic based on module settings
-              vmConfig = self.nixosConfigurations.vm.config;
-              runnerPath = vmConfig.microvm.declaredRunner.outPath;
-
-              script = pkgs.writeShellScriptBin "launch-agent" ''
-                set -e
-
-                ${vmConfig.agentspace.sandbox.initExtra}
-
-                echo "🖥️  Running Agent..."
-                exec "${runnerPath}/bin/microvm-run"
-              '';
-            in
-            "${script}/bin/launch-agent";
+          program = "${mkLaunchScript "vm"}/bin/launch-agent-vm";
         };
       };
     };
